ba2a3f7c5852d56257fc632d8f5cd6bf1dcbe6ee
Chris <crmackay@gmail.com>
2014-02-24 22:26:31 -0500

    udate

--- form_app/client_side/js/navigation.js
+++ form_app/client_side/js/navigation.js
@@ -16,3 +16,20 @@ function nav(){
 
 
 
+function displayPage(page) {
+	// Create the <style> tag
+	var style = document.createElement("style");
+
+	// Add a media (and/or media query) here if you'd like!
+	// style.setAttribute("media", "screen")
+	// style.setAttribute("media", "@media only screen and (max-width : 1024px)")
+
+	// WebKit hack :(
+	style.appendChild(document.createTextNode(""));
+
+	// Add the <style> element to the page
+	document.head.appendChild(style);
+
+	return style.sheet;
+}
+
