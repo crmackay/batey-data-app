69300b94cecfab9e366b8266cb530e9f9c3af8c3
Chris MacKay <crmackay@users.noreply.github.com>
2014-10-17 15:14:40 -0400

    Update save_to_mysql.py

--- form_app/server_side/save_to_mysql.py
+++ form_app/server_side/save_to_mysql.py
@@ -1,4 +1,5 @@
 #load config.py (which connects to the database)
-#recievec http POST data
-    #http://www.tutorialspoint.com/python/python_cgi_programming.htm
+#recieve http POST data via FLASK-framework app
+
 # enter it into the appropriate database
+# return HTTP 201 or 4xx
